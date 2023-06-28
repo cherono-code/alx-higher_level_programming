702f49564243debf8a4df3fceb0d1cb879cc9a4f
cherono-code <cheronodaisy38@gmail.com>
2023-06-28 11:57:06 -0700

    corrections

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defining a class Square."""
+"""Define a class Square."""
 
 
 class Square:
+    """Represent a square."""
     pass
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""Defining a class Square."""
 
 
 class Square:
+    """Represents a square."""
 
     def __init__(self, size=0):
+        """Initializes a new Square.
 
+        Args:
+            size (int): size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
