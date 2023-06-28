1bb6a9ecc835aea6d88518d1d13c88560feda15b
cherono-code <cheronodaisy38@gmail.com>
2023-06-28 05:04:11 -0700

    squares

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializing a new Square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        self.__size = size
