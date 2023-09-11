f0194f105a590a4fc66858d167bb97beeb2dbdf5
cherono-code <cheronodaisy38@gmail.com>
2023-09-11 12:09:40 -0700

    warm up tasks

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,2 @@
+#!/usr/bin/node
+exports.add = (e, f) => e + f;
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+function add (e, f {
+  return e + f;
+}
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
